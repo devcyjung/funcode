c27ad97dfa1f936d387423798f7db23764458d71
chiyoung jung <jcy0011@yahoo.com>
2025-03-07 16:58:56 +0900

    refactor: moved useSyncExternalStore argument callbacks outside of the hook to avoid rerender

--- src/dom/usePrefersDark.tsx
+++ src/dom/usePrefersDark.tsx
@@ -1,21 +1,22 @@
 import {useSyncExternalStore} from 'react'
 
-export function usePrefersDark(): boolean {
-    const subscribePrefersDark: (cb: () => void) => () => void = (
-        cb: () => void
-    ): (() => void) => {
+const subscribePrefersDark: (cb: () => void) => () => void = (
+    cb: () => void
+): (() => void) => {
+    window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', cb)
+    return (): void => {
         window
             .matchMedia('(prefers-color-scheme: dark)')
-            .addEventListener('change', cb)
-        return (): void => {
-            window
-                .matchMedia('(prefers-color-scheme: dark)')
-                .removeEventListener('change', cb)
-        }
+            .removeEventListener('change', cb)
     }
-    const getPrefersDarkSnapshot: () => boolean = (): boolean => {
-        return window.matchMedia('(prefers-color-scheme: dark)').matches
-    }
+}
 
+const getPrefersDarkSnapshot: () => boolean = (): boolean => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+}
+
+export function usePrefersDark(): boolean {
     return useSyncExternalStore(subscribePrefersDark, getPrefersDarkSnapshot)
 }
